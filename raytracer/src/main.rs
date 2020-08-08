98805124f5583b34308754ce1adbf006f00599aa
hyeonjang <hyeonjang.an@skku.edu>
2020-08-08 15:21:25 +0900

    added 'Add operator overloading'

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -1,3 +1,14 @@
+// extern crate rxmath;
+
+// pub struct Ray {
+//     o : Vector3,
+//     d : Vector3,
+// }
+
+// pub struct Isect {
+
+// }
+
 fn main() {
     println!("main raytracer");
 }
--- rxmath/src/lib.rs
+++ rxmath/src/lib.rs
@@ -8,6 +8,27 @@ pub struct Vector2<T> {
     y : T,
 }
 
+pub struct Vector3<T> {
+    x : T,
+    y : T,
+    z : T,
+}
+
+pub struct Vector4<T> {
+    x : T, 
+    y : T,
+    z : T,
+    w : T,
+}
+
+impl<T: ops::Add<Output = T>> ops::Add<Vector2<T>> for Vector2<T> {
+    type Output = Vector2<T>;
+
+    fn add(self, _rhs: Vector2<T>) -> Vector2<T> {
+        Vector2{ x:self.x+_rhs.x, y:self.y+_rhs.y }
+    }
+}
+
 impl<T: std::clone::Clone+ops::Add<Output=T>+ops::Mul<Output=T>+ops::Div<Output=T>> Vector2<T> {
     pub fn new(_x:T, _y:T) -> Vector2<T> {
         Vector2 { x:_x, y:_y } 
@@ -43,9 +64,11 @@ mod tests {
     fn it_works() {
 
         let vec0 = Vector2::new(2, 2);
+        let vec1 = Vector2::new(3, 3);
+        let vec2 = vec0 + vec1;
         //let vec1 = Vector2::new(3, 3);
         //let vec2 = Vector2::new(5, 5);
-        let vec_norm = vec0.norm2();
-        assert_eq!(vec_norm, 8);
+        let vec_norm = vec2.norm2();
+        assert_eq!(vec_norm, 50);
     }
 }

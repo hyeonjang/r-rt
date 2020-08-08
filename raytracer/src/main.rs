4be932bc15ced135cd9d8a694a61f104d42083e1
hyeonjang <hyeonjang.an@skku.edu>
2020-08-08 17:28:58 +0900

    generic Vector type

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -1,14 +1,22 @@
-// extern crate rxmath;
+extern crate rxmath;
+use rxmath::*;
 
-// pub struct Ray {
-//     o : Vector3,
-//     d : Vector3,
-// }
+pub struct Ray<T> {
+    o : Vector3<T>,
+    d : Vector3<T>,
+}
+
+impl Ray {
+    fn new(_o:Vector3, _d:Vector3) -> Ray {
+        Ray{ o:_o, d:_d }
+    }
+}
 
 // pub struct Isect {
 
 // }
 
 fn main() {
-    println!("main raytracer");
+    let r : Ray = Ray::new(vec3(0,0,0), vec3(1,1,1));
+    println!("main raytracer{}", r.o.x);
 }
--- rxmath/src/lib.rs
+++ rxmath/src/lib.rs
@@ -1,8 +1,7 @@
-//implementing with referencing cgmath
-
 extern crate num_traits;
 use std::ops;
 
+// 0. Vector Structures 
 pub struct Vector2<T> { 
     x : T, 
     y : T,
@@ -21,6 +20,36 @@ pub struct Vector4<T> {
     w : T,
 }
 
+// 0.1.0 Contructors 
+impl<T> Vector2<T> {
+    pub fn new(_x:T, _y:T) -> Vector2<T> {
+        Vector2 { x:_x, y:_y } 
+    }
+}
+
+impl<T> Vector3<T> {
+    pub fn new(_x:T, _y:T, _z:T) -> Vector3<T> {
+        Vector3 { x:_x, y:_y, z:_z } 
+    }
+}
+
+impl<T> Vector4<T> {
+    pub fn new(_x:T, _y:T, _z:T, _w:T) -> Vector4<T> {
+        Vector4 { x:_x, y:_y, z:_z, w:_w }
+    }
+}
+
+// 0.1.1 Function Contructors for Convenience
+pub fn vec2<T>(_x:T, _y:T) -> Vector2<T> {
+    Vector2 { x:_x, y:_y }
+}
+
+pub fn vec3<T>(_x:T, _y:T, _z:T) -> Vector3<T> {
+    Vector3 { x:_x, y:_y, z:_z }
+}
+
+// 1. Operator overloading
+// 1.1 Vector2
 impl<T: ops::Add<Output = T>> ops::Add<Vector2<T>> for Vector2<T> {
     type Output = Vector2<T>;
 
@@ -29,32 +58,57 @@ impl<T: ops::Add<Output = T>> ops::Add<Vector2<T>> for Vector2<T> {
     }
 }
 
-impl<T: std::clone::Clone+ops::Add<Output=T>+ops::Mul<Output=T>+ops::Div<Output=T>> Vector2<T> {
-    pub fn new(_x:T, _y:T) -> Vector2<T> {
-        Vector2 { x:_x, y:_y } 
-    }
+impl<T: ops::Sub<Output = T>> ops::Sub<Vector2<T>> for Vector2<T> {
+    type Output = Vector2<T>;
 
+    fn sub(self, _rhs: Vector2<T>) -> Vector2<T> {
+        Vector2{ x:self.x-_rhs.x, y:self.y-_rhs.y }
+    }
+}
+
+// 2. Methods for Vectors
+// 2.1 Vector2
+impl<Float: num_traits::Float + std::clone::Clone+ops::Add<Output=Float>+ops::Mul<Output=Float>+ops::Div<Output=Float>> Vector2<Float> {
     // norm/length/dot: floating-point only functions
-    pub fn norm2(self) -> T {
-        self.x.clone() * self.x + self.y.clone()* self.y
+    pub fn norm2(self) -> Float {
+        self.x.mul_add(self.x, self.y.clone()* self.y)
     }
-    pub fn norm(self) -> T {
-        self.norm2()
+    pub fn norm(self) -> Float {
+        self.norm2().sqrt()
     }
-    pub fn length2(&self) -> T {
-        self.x.clone() * self.x.clone() + self.y.clone() * self.y.clone()
+    pub fn length2(&self) -> Float {
+        self.x.mul_add(self.x, self.y.clone()* self.y)
     }
-    pub fn length(&self) -> T {
-        self.length2()
+    pub fn length(&self) -> Float {
+        self.length2().sqrt()
     }
-    pub fn dot(self, v:Vector2<T>) -> T {
-        self.x * v.x + self.y * v.y
+    pub fn dot(self, v:Vector2<Float>) -> Float {
+        self.x.mul_add(v.x, self.y * v.y)/self.length()
     }
 }
 
-pub fn vec2<T>(_x:T, _y:T) -> Vector2<T> {
-    Vector2 { x:_x, y:_y }
-}
+// impl<T: std::clone::Clone+ops::Add<Output=T>+ops::Mul<Output=T>+ops::Div<Output=T>> Vector2<T> {
+//     pub fn new(_x:T, _y:T) -> Vector2<T> {
+//         Vector2 { x:_x, y:_y } 
+//     }
+
+//     // norm/length/dot: floating-point only functions
+//     pub fn norm2(self) -> T {
+//         self.x.clone() * self.x + self.y.clone()* self.y
+//     }
+//     pub fn norm(self) -> T {
+//         self.norm2()
+//     }
+//     pub fn length2(&self) -> T {
+//         self.x.clone() * self.x.clone() + self.y.clone() * self.y.clone()
+//     }
+//     pub fn length(&self) -> T {
+//         self.length2()
+//     }
+//     pub fn dot(self, v:Vector2<T>) -> T {
+//         self.x * v.x + self.y * v.y
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
@@ -63,12 +117,12 @@ mod tests {
     #[test]
     fn it_works() {
 
-        let vec0 = Vector2::new(2, 2);
-        let vec1 = Vector2::new(3, 3);
+        let vec0 = Vector2::new(2_f32, 2_f32);
+        let vec1 = Vector2::new(3_f32, 3_f32);
         let vec2 = vec0 + vec1;
         //let vec1 = Vector2::new(3, 3);
         //let vec2 = Vector2::new(5, 5);
         let vec_norm = vec2.norm2();
-        assert_eq!(vec_norm, 50);
+        assert_eq!(vec_norm, 50_f32);
     }
 }

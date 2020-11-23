0b5c076580c5875eec17960758e1ac2bc0498e2d
hyeonjang <hyeonjang.an@skku.edu>
2020-11-23 23:23:33 +0900

    @@Todo threading

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -1,5 +1,9 @@
 extern crate image;
 
+use std::sync::Arc;
+use std::thread;
+use std::borrow::Borrow;
+
 pub mod intersect;
 pub mod shape;
 pub mod camera;
@@ -55,30 +59,35 @@ fn main() {
     let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
 
     // World
-    let mut world : ShapeList<Sphere> = ShapeList::new();
+    let mut world = ShapeList::new();
     world.push( Sphere{center:vec3(0f64, 0f64, -1f64), radius:0.5f64} );
     world.push( Sphere{center:vec3(-1.0f64, 0f64, -1f64), radius:0.5f64} );
     world.push( Sphere{center:vec3(0f64, 100.5f64, -1f64), radius:100.0f64} );
+
+    let mut ArcWorld = Arc::new(world);
+    let ArcImgbuf = Arc::new(imgbuf);
 
     // Camera
     let cam = Camera::new();
  
     // A redundant loop to demonstrate reading image data
-    for y in (0..imgy).rev() {
-        for x in 0..imgx {
-            let mut pixel_color = vec3(0.0, 0.0, 0.0);
-            for _i in 0..sample_count {
-                let u  = (x as f64 + random_f64())/(imgx-1) as f64;
-                let v  = (y as f64 + random_f64())/(imgy-1) as f64;        
-                let r : ray = cam.get_ray(u, v);
-                pixel_color += ray_color(r, &mut world, MAX_DEPTH);
+    let handle = thread::spawn(move || {
+        for y in (0..imgy).rev() {
+            for x in 0..imgx {
+                let mut pixel_color = vec3(0.0, 0.0, 0.0);
+                for _i in 0..sample_count {
+                    let u  = (x as f64 + random_f64())/(imgx-1) as f64;
+                    let v  = (y as f64 + random_f64())/(imgy-1) as f64;        
+                    let r : ray = cam.get_ray(u, v);
+                    pixel_color += ray_color(r, &mut ArcWorld, MAX_DEPTH);
+                }
+                let rgb = write_color(pixel_color, sample_count);
+                let pixel = ArcImgbuf.get_pixel_mut(x, y);
+                *pixel = image::Rgb([rgb.x as u8, rgb.y as u8, rgb.z as u8]);
             }
-            let rgb = write_color(pixel_color, sample_count);
-            let pixel = imgbuf.get_pixel_mut(x, y);
-            *pixel = image::Rgb([rgb.x as u8, rgb.y as u8, rgb.z as u8]);
         }
-    }
+    });
 
     // Save the image as “fractal.png”, the format is deduced from the path
-    imgbuf.save("result.png").unwrap();
+    //imgbuf.save("result.png").unwrap();
 }
